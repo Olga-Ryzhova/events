8023addad2cdfb46750b8142201bc95988b3b31a
Olga Ryzhova <olya_saleeva@mail.ru>
2024-06-23 16:59:43 +0400

    вторая версия домашней работы

--- src/js/game.js
+++ src/js/game.js
@@ -31,6 +31,27 @@ function goGnome() {
       deactivateGnome(activeGnome);
       activeGnome = Math.floor(1 + Math.random() * 16);
       activateGnome(activeGnome);
+
+      // Добавляем проверку нажатия на гнома через одну секунду после его появления
+      const dead = document.getElementById("dead");
+      const lost = document.getElementById("lost");
+      let isClicked = false;
+      setTimeout(() => {
+        if (!isClicked) {
+          isClicked = true;
+          if (getGnome(activeGnome).classList.contains("gnome_has-mole")) {
+            lost.textContent++;
+            getGnome(activeGnome).classList.remove("gnome_has-mole");
+          }
+
+          // Проверяем, проиграл ли игрок
+          if (lost.textContent == 5) {
+            alert("Вы проиграли");
+            dead.textContent = 0;
+            lost.textContent = 0;
+          }
+        }
+      }, 1000); // Задержка в одну секунду перед проверкой нажатия
       next();
     }, 1000);
 
--- src/js/logic.js
+++ src/js/logic.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dead = document.getElementById("dead");
-  const lost = document.getElementById("lost");
 
   for (let i = 1; i <= 16; i++) {
     const getGnome = (index) => document.getElementById(`gnome${index}`);
@@ -10,14 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
       if (gnome.classList.contains("gnome_has-mole")) {
         dead.textContent++;
         event.target.classList.remove("gnome_has-mole");
-      } else {
-        lost.textContent++;
-      }
-
-      if (lost.textContent == 5) {
-        alert("Вы проиграли");
-        dead.textContent = 0;
-        lost.textContent = 0;
       }
     });
   }
